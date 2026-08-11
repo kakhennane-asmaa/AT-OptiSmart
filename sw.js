3b04a53720ce969bef17bf511500ed52617e243b
KHENNANE kheira asmaa <ka.khennane@cu-elbayadh.dz>
2026-08-11 17:57:32 +0100

    Add service worker with caching functionality
    Implement service worker for caching assets

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
+const CACHE_NAME = 'geofiber-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html'
+];
 
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
